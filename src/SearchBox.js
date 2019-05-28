026e15eb490850cb2eff64e4794ff413e6209005
rms-sth <ramesrestpradhan@gmail.com>
2019-05-28 22:44:55 +0545

    Before state

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearhBox = () => {
+	return(
+		<div className="pa2">
+			<input className="pa3 ba b--blue bg-lightest-blue" type="search" placeholder="Type to filter robot friends..." />
+		</div>
+	)
+}
+
+export default SearhBox;
